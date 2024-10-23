7afd2f3889b316a0337ea6dbfa375599e1695451
MURILO VIEIRA CAETANO <murilo.vcaetano@senacsp.edu.br>
2024-10-23 17:00:46 -0300

    Copia do erro

--- SysTINSApp/FrmUsuarios.cs
+++ SysTINSApp/FrmUsuarios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SysTINSClass;
 
 namespace SysTINSApp
 {
@@ -15,6 +16,70 @@ namespace SysTINSApp
         public FrmUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbNivel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmUsuarios_Load(object sender, EventArgs e)
+        {
+            cmbNivel.DataSource = Nivel.ObterLista();//uma lista ou um objeto do tipo lista de niveis contendo todos os objetos do tipo lista
+            cmbNivel.DisplayMember = "Nome";
+            cmbNivel.ValueMember = "Id";
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
